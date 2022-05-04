abb244642964ef6c24cdbde83f7d87a6a6338a90
ju0 <0802ryu@gmail.com>
2022-05-04 10:40:24 +0900

    fragment

--- app/src/main/java/com/example/solarpred/SplashActivity.java
+++ app/src/main/java/com/example/solarpred/SplashActivity.java
@@ -33,4 +33,30 @@ public class SplashActivity extends AppCompatActivity {
             }
         },2500);
     }
+
+    public static class DataPage {
+        int color;
+        String title;
+
+        public DataPage(int color, String title){
+            this.color = color;
+            this.title = title;
+        }
+
+        public int getColor() {
+            return color;
+        }
+
+        public void setColor(int color) {
+            this.color = color;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+    }
 }
